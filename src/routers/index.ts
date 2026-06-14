95c41c961b42fd727193d2f53946f5f912bc1d08
Roei <roygim@gmail.com>
2026-06-14 11:02:43 +0300

    roeig

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,6 +3,16 @@ import usersRouter from "./users.router";
 
 const routers = Router()
 
+/**
+ * @swagger
+ * /healthz:
+ *   get:
+ *     tags: [Health]
+ *     summary: Health check
+ *     responses:
+ *       200:
+ *         description: ok
+ */
 routers.get('/healthz', (req, res) => { return res.status(200).json('ok') })
 
 routers.use(usersRouter)
